327cfe2aa32596ecadef7e65d4ed4d80de40ef4d
FernandoJ27 <fjjc27@gmail.com>
2016-06-07 19:52:18 -0500

    Ez Controllers

---
+++ Laboratorio11_MVC_BD_Jiménez/Controllers/PedidoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio11_MVC_BD_Jiménez.Controllers
+{
+    public class PedidoController : Controller
+    {
+        // GET: Pedido
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
